5af03ee8f52a58327059e36cc38199f925efe310
James Lemieux <jrlmx2@gmail.com>
2024-09-01 07:31:32 -0400

    fixed maps link and removed deprecated file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,13 +14,10 @@ const body: string = encodeURIComponent('Me gustaría saber más sobre el progra
 })
 
 export class AppComponent {
-  title = 'esl-web'
-  latlong = '36.0840764,-79.2800624'
-  iosLink = `http://maps.apple.com/?daddr=${this.latlong}&t=r`
+  iosLink = `https://maps.apple.com/?address=939%20S%20Third%20St,%20Mebane,%20NC%20%2027302,%20United%20States&auid=9617755614119622727&ll=36.084097,-79.277507&lsp=9902&q=Grace%20Reformed%20Baptist%20Church&t=r`
   otherLink = 'https://www.google.com/maps/place/Grace+Reformed+Baptist+Church/@36.0840764,-79.2800624,17z/data=!3m1!4b1!4m6!3m5!1s0x89acd6881d6dddd9:0xd9256638f037e904!8m2!3d36.0840764!4d-79.2774875!16s%2Fg%2F1tggbvg5?entry=ttu'
 
-  constructor(private readonly platform: Platform) {
-  }
+  constructor(private readonly platform: Platform) {}
 
   openMaps() {
     let url = this.platform.IOS ? this.iosLink : this.otherLink;
