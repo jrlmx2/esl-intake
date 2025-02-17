ec63b4154ad5e2d0ce072876b7774bfd5bd9630b
James Lemieux <jrlmx2@gmail.com>
2025-02-17 18:18:21 -0500

    added GTM

--- src/app/title.service.ts
+++ src/app/title.service.ts
@@ -1,29 +1,49 @@
-import { Injectable } from '@angular/core'
+import { Injectable, OnDestroy } from '@angular/core'
 import { Title } from '@angular/platform-browser'
-import { RouterStateSnapshot, TitleStrategy } from '@angular/router'
+import {
+    NavigationEnd,
+    Router,
+    RouterStateSnapshot,
+    TitleStrategy
+} from '@angular/router'
 import { GoogleTagManagerService } from 'angular-google-tag-manager'
+import { filter, Subscription } from 'rxjs'
+import { environment } from './environment'
+
+declare var gtag: any
 
 @Injectable({
     providedIn: 'root'
 })
-export class TitleService extends TitleStrategy {
+export class TitleService extends TitleStrategy implements OnDestroy {
+    subHandle: Subscription
+
     constructor(
         private title: Title,
-        private gtm: GoogleTagManagerService
+        private gtm: GoogleTagManagerService,
+        router: Router
     ) {
         super()
+
+        this.subHandle = router.events
+            .pipe(filter(e => e instanceof NavigationEnd))
+            .subscribe((e: NavigationEnd) => {
+                gtag('config', environment.gtag, {
+                    page_path: e.urlAfterRedirects
+                })
+            })
     }
 
     updateTitle(snapshot: RouterStateSnapshot): void {
         const title = this.buildTitle(snapshot)
         if (title !== undefined) {
             this.title.setTitle(title)
-            this.gtm
-                .pushTag({
-                    event: 'page',
-                    pageName: snapshot.url
-                })
-                .then()
+        }
+    }
+
+    ngOnDestroy(): void {
+        if (this.subHandle) {
+            this.subHandle.unsubscribe()
         }
     }
 }
